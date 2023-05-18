1b76ea0e768e60ff756f08b067d733dc4ffabd60
darskp <darshanpatgar100@gmail.com>
2023-05-18 11:20:20 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/string-toggle-toolkitnpm install gh-pages --save-dev
+",
   plugins: [react()],
 })
